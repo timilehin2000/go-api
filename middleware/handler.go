5ba9affaf7b6c483df2121b38daf8071bfef9d14
timilehin <joeltimmy7@gmail.com>
2022-07-15 21:44:55 +0100

    initial commit

---
+++ middleware/handler.go
@@ -0,0 +1,71 @@
+package middleware
+
+import (
+	"database/sql"
+	"fmt"
+	"goApi/models"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadDb() *sql.DB {
+	//laod the app's environment
+	err := godotenv.Load()
+
+	//flag an err if the env refuses to load
+	if err != nil {
+		log.Fatalf("Error loading %s", err)
+	}
+
+	//load the env variables
+	username := os.Getenv("APP_DB_USERNAME")
+	password := os.Getenv("APP_DB_PASSWORD")
+	host := os.Getenv("APP_DB_HOST")
+	port := os.Getenv("APP_DB_PORT")
+	dbname := os.Getenv("APP_DB_NAME")
+
+	//create the connection string
+	connects := fmt.Sprintf("%s=host %s=port  %s=username "+"%s=password %s=dbname sslmode=disable", host, port, dbname, username, password)
+
+	fmt.Println(connects)
+
+	//connect the database
+	db, err := sql.Open("postgres", connects)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Dabase connected successfully...")
+
+	return db
+}
+
+func GetAllBooks(w http.ResponseWriter, r *http.Request) {
+	var book models.Book
+
+	books := []models.Book{}
+
+	db := LoadDb()
+
+	sqlStament := `SELECT * FROM books`
+
+	rows, err := db.Query(sqlStament)
+
+	if err != nil {
+		fmt.Sprintf("error occured while doing this")
+	}
+
+	for rows.Next() {
+		err = rows.Scan(&book.ID, &book.Author, &book.Title, &book.Year)
+	}
+
+}
